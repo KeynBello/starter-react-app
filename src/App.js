6b3ed490515ce9b1878a474c6539d1fc3048ac5c
Andrii Synytsia <96833388+KeynBello@users.noreply.github.com>
2023-11-11 18:39:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello! It's DevOps SoftServe Academy Course
+          Hello! It's DevOps SoftServe|Academy Course
         </p>
         <a
           className="App-link"
